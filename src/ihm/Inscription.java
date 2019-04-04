3b5636f298057b86f9a962fc64afbb69a74d1b2f
hcarlev <hugocarlevaris@gmail.com>
2019-04-04 12:16:51 +0200

    front creer note bis bis

--- src/ihm/Inscription.java
+++ src/ihm/Inscription.java
@@ -33,11 +33,11 @@ public class Inscription extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 584, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 534, Short.MAX_VALUE)
         );
 
         pack();
