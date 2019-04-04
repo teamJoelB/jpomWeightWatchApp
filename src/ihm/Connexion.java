64c8db03a93b808c738b8c6a833f93b12a310dd8
hcarlev <hugocarlevaris@gmail.com>
2019-04-04 13:00:44 +0200

    front creer note bis bis

--- src/ihm/Connexion.java
+++ src/ihm/Connexion.java
@@ -264,7 +264,6 @@ public class Connexion extends javax.swing.JFrame {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new Connexion().setVisible(true);
-                    new Connexion().setVisible(true);
             }
         });
     }
